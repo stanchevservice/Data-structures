3aceb10982cfaf5302eb94b3a9007bb88e2f4a3b
unknown <stanchev.service@gmail.com>
2020-05-03 12:25:25 +0300

    Added node structure

---
+++ DoublyLinkedList.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DoublyLinkedList.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoublyLinkedList.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d66bcb5-a206-4f87-babb-5cb886c71a84")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Node/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Node
+{
+    public class Node<T>
+    {
+        public T Value { get; private set; }
+
+        public Node<T> Next { get; set; }
+
+        public Node<T> Prev { get; set; }
+
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+
+    }
+}
